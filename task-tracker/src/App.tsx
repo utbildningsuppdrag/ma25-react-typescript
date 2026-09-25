6e398710d9dbd3efe5e8ded668bf7e455cddc421
Santosnr6 <jesper-nyberg@hotmail.com>
2026-09-25 09:48:04 +0200

    feat: initialize task tracker project with React and TypeScript

---
+++ task-tracker/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+	return (
+		<div className="app">
+			<h1 className="title">Task Tracker</h1>
+		</div>
+	);
+}
+
+export default App;
---
+++ task-tracker/src/data/tasks.ts
@@ -0,0 +1,12 @@
+export const initialTasks = [
+	{
+		id: 1,
+		title: "Lära mig TypeScript",
+		completed: false,
+	},
+	{
+		id: 2,
+		title: "Lära mig React",
+		completed: true,
+	},
+];
